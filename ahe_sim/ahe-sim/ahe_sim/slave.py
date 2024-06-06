22e88975f1e94c5acf1184b14a5040be443540ac
anusha-ahe <anusha.nss@gmail.com>
2024-06-06 12:05:07 +0530

    changes to docker compose

--- ahe_sim/ahe-sim/ahe_sim/slave.py
+++ ahe_sim/ahe-sim/ahe_sim/slave.py
@@ -5,16 +5,29 @@ import subprocess
 ip = '0.0.0.0'
 
 def is_server_running(port):
-    cmd = f"netstat -tln | grep {ip}:{port}"
-    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    return result.returncode == 0
+    cmd = f"netstat -tlnp | grep {ip}:{port}"
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    if result.returncode == 0:
+        netstat_output = result.stdout.strip().split()
+        pid = netstat_output[-1].split('/')[0]
+        return True, int(pid)
+    return False, None
+
+
+def force_kill_server(port):
+    result, pid = is_server_running(port)
+    if result:
+        print(f"Server for map {name} is already running @ {ip}:{port}")
+        try:
+            os.kill(pid, signal.SIGTERM)
+            print(f"Killed server for port {pid}")
+        except Exception as e:
+            print(f"Failed to kill server with PID {pid}@{port}: {e}")
 
 
 def run_slave(server_context, port, name):
     identity = ModbusDeviceIdentification()
-    if is_server_running(port):
-        print(f"Server for map {name} is already running @ {ip}:{port}")
-        return
+    force_kill_server(port)
     try:
         print(f"Starting server for map {name} @ {ip}:{port}")
         StartTcpServer(context=server_context, identity=identity, address=(ip, port))
